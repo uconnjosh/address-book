4a9f45f19285a19190cf852a0baf8e9dcbcc5f77
epicodus <student@epicodus.com>
2014-02-24 10:14:11 -0800

    added address book functionality

--- spec/specs.js
+++ spec/specs.js
@@ -1,36 +1,20 @@
-describe('sentence', function() {
-  it('converts one or more words to Pig Latin', function() {
-    sentence("quail good and square").should.equal("ailquay oodgay anday aresquay");
+describe('Contact', function() {
+  describe("fullName", function() {
+    it("combines the first and last name, separated by a space", function() {
+      var testContact = Object.create(Contact);
+      testContact.firstName = "Michael";
+      testContact.lastName = "Jackson";
+      testContact.fullName().should.equal("Michael Jackson");
+    });
   });
-});
-
-describe('pigLatin', function() {
-  it('for words that start with a vowel add "ay" to the end', function() {
-    pigLatin("end").should.equal("enday");
-  });
-
-  it('for words that start with a single consonant, move the first letter to the end and add "ay"', function() {
-    pigLatin("good").should.equal("oodgay");
-  });
-
-   it('for words that start with multiple consonants, move the consonants to the end and add "ay"', function() {
-    pigLatin("strip").should.equal("ipstray");
-  });
-
-   it('for words that start with "qu", move the "q" and the "u" to the end and add "ay"', function() {
-    pigLatin("quail").should.equal("ailquay");
-  });
-
-   it('for words that contain "qu" before the first vowel, move the first consonant and "q" and "u" to the end and add "ay"', function() {
-    pigLatin("shquare").should.equal("areshquay");
-  });
-
-    it('for words that start with "y", move the "y" to the end and add "ay"', function() {
-    pigLatin("yes").should.equal("esyay");
-  });
-
-     it('for words that have "y" not as the first letter treat as vowel', function() {
-    pigLatin("system").should.equal("ystemsay");
+   describe("address", function() {
+    it("prints out the object's address", function() {
+      var testContact = Object.create(Contact);
+      testContact.firstName = "Michael";
+      testContact.lastName = "Jackson";
+      testContact.address = "Hollywood Hills Cemetary";
+      testContact.address.should.equal("Hollywood Hills Cemetary");
+    });
   });
 });
 
